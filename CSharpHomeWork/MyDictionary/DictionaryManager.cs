87eb3cf8c8a0701982ee7771ef69f2d779361887
YUNUS EMRE ULUSAN <19030411029@ybu.edu.tr>
2021-01-21 17:45:06 +0300

    HW4

---
+++ CSharpHomeWork/MyDictionary/DictionaryManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryProject
+{
+    class DictionaryManager<TPageNumber, TValue>
+    {
+        Dictionary<int, string> dictionaryManager = new Dictionary<int, string>();
+        public void Add(int PageNumber, string Value) 
+        {
+
+        }
+        public DictionaryManager()
+            {
+
+            }
+                                  
+        }
+        
+    }
+
---
+++ CSharpHomeWork/MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> bookDatabase = new Dictionary<int, string>();
+            bookDatabase.Add(130, "Satranç");
+            bookDatabase.Add(345 ,"Şeker Portakalı");
+
+            Dictionary<int, string>.KeyCollection ValueList = bookDatabase.Keys;
+            foreach(KeyValuePair<int, string> item in bookDatabase)
+            {
+                Console.WriteLine("PageNumber: {0} , BookName: {1}", item.Key,item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
